558870a6aafa530d20473a886810fbc1e977a889
Ays1004 <yewaleayush@gmail.com>
2025-06-08 17:10:48 +0530

    test

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ type ViewMode = "summary" | "answers";
 //Tags
 //Re-Parse if the model is updated
 //Export Answers as a pdf document
-//Handwritten Pdf scan
+//Handwritten Pdf scan 
 
 export default function Home() {
     const [file, setFile] = useState<File | null>(null);
