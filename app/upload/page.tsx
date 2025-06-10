39ebcc8cb3a197efb3fb746c3244f54a58b767ac
Ays1004 <yewaleayush@gmail.com>
2025-06-10 22:02:32 +0530

    updated upload route

---
+++ app/upload/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-black text-2xl font-semibold text-white h-screen w-screen items-center flex justify-center'>
+        Under Construction
+    </div>
+  )
+}
+
+export default page
